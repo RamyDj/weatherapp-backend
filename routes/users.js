d19a9d6ab2a8cdb1aff7ffc60e21f6464bed2334
Ramy <ramyto.rd@gmail.com>
2024-06-30 13:33:35 +0200

    backend deployement

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+const User = require('../models/users');
+const { checkBody } = require("../modules/checkBody");
+
+//const OWM_API_KEY = 'ce7418650c86eae6629dfcfdda141c14';
+
+
+router.post('/signup', (req, res) => {
+    const existingUser = User.findOne({ email: req.body.email });
+    if (!checkBody(req.body, ["name", "email", "password"])) {
+        res.json({ result: false, error: 'Missing or empty fields' })
+    } else if (existingUser) {
+        res.json({ result: false, error: 'User already exists' })
+    } else {
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        newUser.save().then(() => {
+            res.json({ result: true });
+        });
+    }
+
+});
+
+router.post('/signin', (req, res) => {
+    if (!checkBody(req.body, ["email", "password"])) {
+        res.json({ result: false, error: 'Missing or empty fields' })
+        return
+    }
+
+    User.findOne({ email: req.body.email, password: req.body.password }).then(data => {
+        if (data) {
+            res.json({ result: true });
+        } else {
+            res.json({ result: false, error: "User not found" });
+        }
+    })
+})
+
+module.exports = router;
